2e3da15c5b73a4f2313d5105f0f2bb676454ebe3
Javier Hernandez <jhroncancio@indra.es>
2021-04-27 22:48:29 -0500

    Commit inicial con Eclipse

---
+++ pharmanet/src/main/java/com/drogueria/pharmanet/PharmanetApplication.java
@@ -0,0 +1,13 @@
+package com.drogueria.pharmanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmanetApplication.class, args);
+	}
+
+}
